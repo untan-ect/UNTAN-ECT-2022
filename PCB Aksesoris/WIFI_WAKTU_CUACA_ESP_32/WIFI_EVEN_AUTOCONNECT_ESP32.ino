fee7fc757dc8282d621f35556c936338917c517b
UNTAN-ECT <97281880+untan-ect@users.noreply.github.com>
2022-04-22 14:37:38 +0700

    Update WIFI_EVEN_AUTOCONNECT_ESP32.ino

--- PCB Aksesoris/WIFI_WAKTU_CUACA_ESP_32/WIFI_EVEN_AUTOCONNECT_ESP32.ino
+++ PCB Aksesoris/WIFI_WAKTU_CUACA_ESP_32/WIFI_EVEN_AUTOCONNECT_ESP32.ino
@@ -1,50 +1,56 @@
 #include <WiFi.h>
  
-const char* ssid = "AMS";
-const char* password = "87654321";
+const char* ssid = "***********";
+const char* password = "***********";
 
 void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
-  Serial.println("Connected to AP successfully!");
+  Serial.println("Berhasil terhubung ke WiFi");
 }
 
 void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info){
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.println("WiFi terhubung");
+  Serial.println("Alamat IP: ");
   Serial.println(WiFi.localIP());
 }
 
 void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
-  Serial.println("Disconnected from WiFi access point");
-  Serial.print("WiFi lost connection. Reason: ");
+  Serial.print("WiFi terputus. Alasan: ");
   Serial.println(info.disconnected.reason);
-  Serial.println("Trying to Reconnect");
+  Serial.println("Menyambungkan kembali..");
   WiFi.begin(ssid, password);
 }
 
 void setup(){
   Serial.begin(115200);
-
-  // delete old config
   WiFi.disconnect(true);
-
   delay(1000);
 
   WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
   WiFi.onEvent(WiFiGotIP, SYSTEM_EVENT_STA_GOT_IP);
   WiFi.onEvent(WiFiStationDisconnected, SYSTEM_EVENT_STA_DISCONNECTED);
 
-  /* Remove WiFi event
-  Serial.print("WiFi Event ID: ");
-  Serial.println(eventID);
-  WiFi.removeEvent(eventID);*/
+  /* 
+  Remove WiFi event
+   Serial.print("WiFi Event ID: ");
+   Serial.println(eventID);
+   WiFi.removeEvent(eventID);
+  */
 
+  WiFi.mode(WIFI_STA);                                          // Berperan sebagai stasiun WiFi                                                                                  
   WiFi.begin(ssid, password);
-    
-  Serial.println();
-  Serial.println();
-  Serial.println("Wait for WiFi... ");
+
+  Serial.print("Menghubungkan ke WiFi..");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("Alamat IP: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 void loop(){
   delay(1000);
 }
+
+// Fadli Amman Siwaka - 20 April 2022
